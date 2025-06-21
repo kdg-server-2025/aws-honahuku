5cba9d32254edee2c73933df2cb621a7f8b62516
Honahuku <tech@honahuku.com>
2025-06-21 12:56:13 +0900

    fix: wget でネットワークに疎通できないときのエラー処理を追加

--- configure-ssh.sh
+++ configure-ssh.sh
@@ -4,11 +4,15 @@ set -o pipefail
 
 # GitHub に登録した公開鍵をダウンロードする
 GITHUB_USERNAME="honahuku"
-wget https://github.com/${GITHUB_USERNAME}.keys
+if ! wget --tries=50 --waitretry=10 --connect-timeout=10 -O authorized_keys https://github.com/${GITHUB_USERNAME}.keys
+then
+  # SSH の公開鍵を配置する
+  mkdir -p ~/.ssh/
+  mv authorized_keys ~/.ssh/
 
-# SSH の公開鍵を配置する
-mkdir -p ~/.ssh/
-mv ${GITHUB_USERNAME}.keys ~/.ssh/authorized_keys
-
-# sshd が使えるようにするために鍵のパーミッションを変更する
-chmod 600  ~/.ssh/authorized_keys
+  # sshd が使えるようにするために鍵のパーミッションを変更する
+  chmod 600  ~/.ssh/authorized_keys
+  exit 0
+else
+  exit 1
+fi
