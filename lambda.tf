01c70b13a64859f0508fc23b4059e8fdedb3e8b6
Honahuku <tech@honahuku.com>
2025-06-28 12:50:46 +0900

    feat: go のコードがデプロイ出来た

--- lambda.tf
+++ lambda.tf
@@ -1,8 +1,8 @@
 # CI/CD側でlambdaのソースコードを格納するための箱
-resource "aws_s3_bucket" "kdg_2025_lambda_yamakawa" {
-  bucket = "kdg-2025-lambda-yamakawa"
+resource "aws_s3_bucket" "lambda_artifacts" {
+  bucket = "kdg-aws-2025-honahuku-lambda-artifacts"
   tags = {
-    Name = "kdg-2025-lambda-yamakawa"
+    Name = "kdg-aws-2025-honahuku-lambda-artifacts"
   }
 }
 
@@ -26,32 +26,22 @@ resource "aws_iam_role" "lambda" {
 EOF
 }
 
-# 初回のみ利用する空のLambdaのファイルを生成
-data "archive_file" "initial_lambda_package" {
-  type        = "zip"
-  output_path = "${path.module}/.temp_files/lambda.zip"
-  source {
-    content  = "# empty"
-    filename = "main.py"
-  }
-}
-
 # 生成した空のLambdaのファイルをS3にアップロード
 resource "aws_s3_object" "lambda_file" {
-  bucket = aws_s3_bucket.kdg_2025_lambda_yamakawa.id
+  bucket = aws_s3_bucket.lambda_artifacts.id
   key    = "initial.zip"
   source = "${path.module}/.temp_files/lambda.zip"
 }
 
 # Lambda関数を生成
 # ソースコードは空のLambdaのファイルのS3を参照
-resource "aws_lambda_function" "lambda_test" {
-  function_name = "lambda_test"
+resource "aws_lambda_function" "first_function" {
+  function_name = "first-function"
   role          = aws_iam_role.lambda.arn
   handler       = "main.handler"
   runtime       = "provided.al2023"
   timeout       = 120
   publish       = true
-  s3_bucket     = aws_s3_bucket.kdg_2025_lambda_yamakawa.id
+  s3_bucket     = aws_s3_bucket.lambda_artifacts.id
   s3_key        = aws_s3_object.lambda_file.id
 }
--- lambda_deploy.sh
+++ lambda_deploy.sh
@@ -1,22 +1,23 @@
-# ディレクトリ情報の取得
-CURRENT_PATH=$(cd $(dirname $0); pwd)
-ROOT_PATH=$CURRENT_PATH/..
+#!/bin/bash
+set -exo pipefail
 
-# pythonパッケージのインストール
-pip install --target ./package -r $ROOT_PATH/requirements.txt --system
+# mktemp で作業用のディレクトリを作成 (カレントディレクトリが汚れないようにするため, 不要なファイルが zip に入らないようにするため)
+TEMPDIR=$(mktemp -d)
+# 各自のバケット名に書き換え
+ARTIFACT_BUCKET="kdg-aws-2025-honahuku-lambda-artifacts"
 
-# pythonパッケージのzip化
-cd package
-zip ../deployment-package.zip -r ./*
+# # ソースコードをzipファイルに追加
+cd function
+go build -tags lambda.norpc -o bootstrap main.go
+
+# function ディレクトリごと zip に含めてしまうのでここで実行
+zip "$TEMPDIR"/deployment-package.zip -r ./bootstrap
+
 cd ..
 
-# ソースコードをzipファイルに追加
-zip -g deployment-package.zip -j $ROOT_PATH/src/main.py
-
 # S3にアップロードしてlambda関数の参照を書き換える
-aws s3 cp ./deployment-package.zip s3://okamoto-tf-test-bucket/
-aws lambda update-function-code --function-name lambda_test --s3-bucket okamoto-tf-test-bucket --s3-key deployment-package.zip
+aws s3 cp "$TEMPDIR"/deployment-package.zip s3://$ARTIFACT_BUCKET/
+aws lambda update-function-code --no-cli-pager --function-name first-function --s3-bucket $ARTIFACT_BUCKET --s3-key deployment-package.zip
 
 # デプロイ時の一時ファイルを削除
-rm deployment-package.zip
-rm -r package
+rm -rf "$TEMPDIR"
