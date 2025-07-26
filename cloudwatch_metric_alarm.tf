3d877c9fdaca3776f8c703093c852daddaa096f8
Honahuku <tech@honahuku.com>
2025-07-26 14:58:27 +0900

    fix: lamda の rename を aws_cloudwatch_metric_alarm でも対応

--- cloudwatch_metric_alarm.tf
+++ cloudwatch_metric_alarm.tf
@@ -1,7 +1,7 @@
 # 1分間のエラー数が2回以上になったらアラームを発報する
 resource "aws_cloudwatch_metric_alarm" "lambda_error" {
-  alarm_name          = "${aws_lambda_function.first_function.function_name}-high-error-count-alarm"
-  alarm_description   = "${aws_lambda_function.first_function.function_name} でエラー発生回数が増えています"
+  alarm_name          = "${aws_lambda_function.kdg_lamda_sample.function_name}-high-error-count-alarm"
+  alarm_description   = "${aws_lambda_function.kdg_lamda_sample.function_name} でエラー発生回数が増えています"
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = 1
   metric_name         = "Errors"
@@ -12,20 +12,20 @@ resource "aws_cloudwatch_metric_alarm" "lambda_error" {
   threshold = 2
 
   dimensions = {
-    FunctionName = aws_lambda_function.first_function.function_name
+    FunctionName = aws_lambda_function.kdg_lamda_sample.function_name
   }
 
   alarm_actions = [aws_sns_topic.lambda_alarm_topic.arn]
   ok_actions    = [aws_sns_topic.lambda_alarm_topic.arn]
 
   tags = {
-    Name = "${aws_lambda_function.first_function.function_name}-high-error-count-alarm"
+    Name = "${aws_lambda_function.kdg_lamda_sample.function_name}-high-error-count-alarm"
   }
 }
 
 # アラーム通知用のSNSトピックとサブシクリプション
 resource "aws_sns_topic" "lambda_alarm_topic" {
-  name = "${aws_lambda_function.first_function.function_name}-alarm-topic"
+  name = "${aws_lambda_function.kdg_lamda_sample.function_name}-alarm-topic"
 }
 resource "aws_sns_topic_subscription" "lambda_alarm" {
   topic_arn = aws_sns_topic.lambda_alarm_topic.arn
